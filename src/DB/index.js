27eb19f0c2f212d70d35fe4a458d43b63ade98db
Ragib Shariar <srragib@gmail.com>
2024-06-22 21:19:32 +0600

    database connection complete

--- src/DB/index.js
+++ src/DB/index.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-
     console.log(
       `MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
     );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-// require('dotenv').config({ path: './env' });
+// require('dotenv').config({ path: './.env' });
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./DB/index.js";
+import { app } from "./app.js";
 
-const app = express();
-dotenv.config({ path: "./env" });
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 5000;
 
 // MongoDB database connection
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`⚙️  Server is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!!!", err);
+  });
+
+
 
 /* 
   not an efficient way to connect to the database
